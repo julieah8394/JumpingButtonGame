b217ea2d05ee478e056ac7d82be365d20821b622
mah537 <mah537@208-WD42.up.ist.local>
2014-09-23 13:51:43 -0400

    yadda yadda comment

--- src/jumpingbuttongame/JumpingButtonGame.java
+++ src/jumpingbuttongame/JumpingButtonGame.java
@@ -16,7 +16,7 @@ public class JumpingButtonGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // yadda yadda 
     }
     
 }
